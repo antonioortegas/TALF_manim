4462a84593ea1e2ed54fc5c5cfab6051f1e7451e
antonioortegas <antonioortega@uma.es>
2023-10-06 00:25:46 +0200

    first table

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 Estos videos han sido realizados utilizando principalmente la libreria [Manim](https://github.com/3b1b/manim) para python de [3Blue1Brown](https://www.youtube.com/channel/UCYO_jab_esuFRV4b17AJtAw).
 
-Más concretamente, se ha utilizado la versión 
-[Manim Community Edition](https://github.com/ManimCommunity/manim/), un fork de la versión original de 3Blue1Brown pensado para ser más estable, mantenido por un grupo de desarrolladores independientes.
+Más concretamente, se ha utilizado la versión [Manim Community Edition](https://github.com/ManimCommunity/manim/), un fork de la versión original de 3Blue1Brown pensado para ser más estable, mantenido por un grupo de desarrolladores independientes.
 
 ## Video 1 : Cierre de los Tipos de Lenguajes
--- test.py
+++ test.py
@@ -2,16 +2,32 @@ from manim import *
 
 class ToyExample(Scene):
     def construct(self):
-        orange_square = Square(color=ORANGE, fill_opacity=0.5)
-        blue_circle = Circle(color=BLUE, fill_opacity=0.5)
-        self.add(orange_square)
-        self.play(ReplacementTransform(orange_square, blue_circle, run_time=1))
-        small_dot = Dot()
-        small_dot.add_updater(lambda mob: mob.next_to(blue_circle, DOWN))
-        self.play(Create(small_dot))
-        self.play(blue_circle.animate.shift(RIGHT))
-        self.wait()
-        self.play(FadeOut(blue_circle, small_dot))
+        rowLabels = [Text("Unión"), Text("Intersección"), Text("Intersección con L.3"), Text("Complemento"), Text("Concatenación"), Text("Potencia"), Text("Cierre"), Text("Cierre estricto"), Text("Inverso")]
+        columnLabels = [Text("Estructura de frase"), Text("Sensible al contexto"), Text("Contexo libre"), Text("Lineales"), Text("Regular")]
+        t2 = Table(
+            [
+                ["Sí", "Sí", "Sí", "Sí", "Sí"],
+                ["Sí", "Sí", "No", "No", "Sí"],
+                ["Sí", "Sí", "Sí", "Sí", "Sí"],
+                ["No", "Sí", "No", "No", "Sí"],
+                ["Sí", "Sí", "Sí", "No", "Sí"],
+                ["Sí", "Sí", "Sí", "No", "Sí"],
+                ["Sí", "Sí", "Sí", "No", "Sí"],
+                ["Sí", "Sí", "Sí", "No", "Sí"],
+                ["Sí", "Sí", "Sí", "Sí", "Sí"],
+            ],
+            rowLabels,
+            columnLabels,
+            include_outer_lines=True)
+        t2.get_vertical_lines().set_stroke(width=0.5)
+        t2.get_horizontal_lines().set_stroke(width=0.5)
+        t2.scale(0.25)
+        self.add(t2)
+        self.play(t2.create(line_animation=Write,
+                            element_animation=Create,
+                            label_animation=Write), run_time=3)
+        self.wait(10)
+        
 
 with tempconfig({"quality": "high_quality", "preview": True}):
     scene = ToyExample()
