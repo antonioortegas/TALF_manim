56155020cd15a53576e6f9f61832b43d35a53040
antonioortegas <antonioortega@uma.es>
2023-10-20 16:03:41 +0200

    update

--- Bloque1/test.py
+++ Bloque1/test.py
@@ -4,6 +4,9 @@ class Video(Scene):
     def construct(self):
         
         self.camera.background_color = BLACK
+        audio_path = "Bloque1/audio.wav"
+        self.add_sound(audio_path, time_offset=1.0)
+        self.wait(1)
         
         # Table creation
         rowLabels = [Text("Unión"), Text("Intersección"), Text("Intersección con L.3"), Text("Complemento"), Text("Concatenación"), Text("Potencia"), Text("Cierre"), Text("Cierre estricto"), Text("Inverso")]
@@ -74,29 +77,29 @@ class Video(Scene):
         naturalSet.shift(DOWN*0.8)
         naturalSetLabel = Tex(r"$\mathbb{N}$", color=WHITE).next_to(naturalSet.get_top(), UP)
         
-        self.play(Write(title), run_time=0.5)
-        self.play(FadeOut(title, shift=UP), run_time=1.5)
-        self.play(Write(textEsCerrado), run_time=1.0)
+        self.play(Write(title), run_time=1)
+        self.wait(24)
+        self.play(FadeOut(title, shift=UP), run_time=2)
+        
+        #28
+        self.play(Write(textEsCerrado), run_time=3)
+        self.wait(7)
         self.play(textEsCerrado.animate.shift(UP*2.5), run_time=1.0)
+        
+        #39
         self.play(DrawBorderThenFill(naturalSet), run_time=1.0)
-        self.play(Write(naturalSetLabel), run_time=0.5)
-        
+        self.play(Write(naturalSetLabel), run_time=1)
         pentagon = Polygon(*[naturalSet.point_from_proportion(i/5) for i in range(5)], color=BLUE).scale(0.6)
-        
-        
-        
-        
-        
-        
         
         naturalSetElements = VGroup()
         for i in range(5):
             naturalSetElements.add(Tex(str(i), color=WHITE).move_to(pentagon.get_vertices()[i]))
-        
         naturalSetGroup = VGroup(naturalSet, naturalSetLabel, naturalSetElements)
         
         self.play(Write(naturalSetElements), run_time=1.0)
+        self.wait(2)
         self.play(naturalSetGroup.animate.shift(LEFT*2.75), run_time=1.0)
+        
         
         boxes = VGroup()
         op_box = Rectangle(height=1.5, width=1.5, color=WHITE, fill_opacity=0.2, fill_color=WHITE)
@@ -106,109 +109,135 @@ class Video(Scene):
         boxes.shift(RIGHT*2.75)
         boxes.shift(DOWN*0.8)
         
+        #45
         self.play(DrawBorderThenFill(op_box), run_time=1.0)
         sumSymbol = Tex(r"$+$", color=WHITE).move_to(op_box.get_center()).scale(1.5)
         moreThanOrEqualToZero = Tex(r"$\mathbb{N}?$", color=WHITE).move_to(classification_box.get_center())
         self.play(Write(sumSymbol), run_time=1.0)
+        self.wait(4)
         
         number1 = naturalSetElements[1].copy()
         number3 = naturalSetElements[3].copy()
         
-        self.play(number1.animate.next_to(op_box, LEFT), run_time=1.0)
-        self.play(FadeOut(number1, shift=RIGHT), run_time=1.0)
-        self.play(number3.animate.next_to(op_box, LEFT), run_time=1.0)
-        self.play(FadeOut(number3, shift=RIGHT), run_time=1.0)
+        #51
+        self.play(number1.animate.next_to(op_box, LEFT), run_time=0.5)
+        self.play(FadeOut(number1, shift=RIGHT), run_time=0.5)
+        self.play(number3.animate.next_to(op_box, LEFT), run_time=0.5)
+        self.play(FadeOut(number3, shift=RIGHT), run_time=0.5)
         
         number4 = naturalSetElements[4].copy()
         middlePoint = (op_box.get_center() + classification_box.get_center())/2
         number4.move_to(middlePoint)
         
+        #53
         self.play(FadeIn(number4, shift=DOWN), run_time=1.0)
+        self.wait(2)
         
+        #56
         self.play(DrawBorderThenFill(classification_box), run_time=1.0)
         self.play(Write(moreThanOrEqualToZero), run_time=1.0)
         
+        #58
         self.play(FadeOut(number4, shift=DOWN), run_time=1.0)
         number4.move_to(classification_box.get_center() + LEFT*1.2)
         number4.color = GREEN
         self.play(FadeIn(number4, shift=LEFT), run_time=1.0)
         
         
-        
+        #1:00
         self.play(Indicate(number4), run_time=1.0, color=GREEN)
         self.play(number4.animate.move_to(naturalSetElements[4]), run_time=1.0)
-        self.wait(2)
+        self.wait(3)
+        self.play(Indicate(VGroup(naturalSetElements[2], naturalSetElements[1])), run_time=1.0, color=YELLOW)
+        self.play(Indicate(VGroup(naturalSetElements[0], naturalSetElements[3])), run_time=1.0, color=YELLOW)
+        self.play(Indicate(VGroup(naturalSetElements[1], naturalSetElements[4])), run_time=1.0, color=YELLOW)
+        self.wait(5)
         
         number1 = naturalSetElements[1].copy()
         number3 = naturalSetElements[3].copy()
         
         subSymbol = Tex(r"$-$", color=WHITE).move_to(op_box.get_center()).scale(1.5)
         
+        #1:13
         self.play(FadeTransform(sumSymbol, subSymbol), run_time=1.0)
+        self.wait(1)
         
-        self.play(number1.animate.next_to(op_box, LEFT), run_time=1.0)
-        self.play(FadeOut(number1, shift=RIGHT), run_time=1.0)
-        self.play(number3.animate.next_to(op_box, LEFT), run_time=1.0)
-        self.play(FadeOut(number3, shift=RIGHT), run_time=1.0)
+        #1:15
+        self.play(number1.animate.next_to(op_box, LEFT), run_time=0.5)
+        self.play(FadeOut(number1, shift=RIGHT), run_time=0.5)
+        self.play(number3.animate.next_to(op_box, LEFT), run_time=0.5)
+        self.play(FadeOut(number3, shift=RIGHT), run_time=0.5)
         
         numberMinus2 = Tex(r"$-2$", color=WHITE).move_to(middlePoint)
         
         
-        
+        #1:17
         self.play(FadeIn(numberMinus2, shift=DOWN), run_time=1.0)
+        self.wait(1)
         self.play(FadeOut(numberMinus2, shift=DOWN), run_time=1.0)
         numberMinus2.move_to(classification_box.get_center() + LEFT*1.2)
         numberMinus2.color = RED
         self.play(FadeIn(numberMinus2, shift=LEFT), run_time=1.0)
         
         
-        
+        #1:21
         self.play(Wiggle(numberMinus2), run_time=1.0, color=RED)
         self.play(numberMinus2.animate.next_to(naturalSet, RIGHT), run_time=1.0)
         self.play(Wiggle(numberMinus2), run_time=1.0, color=RED)
-        self.wait(2)
+        self.wait(10)
         
+        #1:34
         scene = VGroup(naturalSetGroup, numberMinus2, subSymbol, textEsCerrado, moreThanOrEqualToZero, number4)
         self.play(FadeOut(scene, shift=UP*5), run_time=1.0)
         self.play(boxes.animate.shift(UP*0.8), run_time=1.0)
+        self.wait(2)
         
-        
+        #1:38
         vennLenguages.shift(LEFT*2.75)
         # Add everything to the scene
         for i in range(5):
-            self.play(DrawBorderThenFill(rings[i]), run_time=1.0)
+            self.play(DrawBorderThenFill(rings[i]), run_time=1.5)
             self.play(Write(labels[i]), run_time=0.5)
        
-        
-        
-        
-        
-        
-        
-        #TODO
+        #1:48
+        self.play(Indicate(rings[0]), run_time=2.0, color=BLUE, scale_factor=1)
+        self.play(Indicate(rings[1]), run_time=2.0, color=BLUE, scale_factor=1)
+        self.play(Indicate(rings[2]), run_time=2.0, color=BLUE, scale_factor=1)
+        self.play(Indicate(rings[3]), run_time=2.0, color=BLUE, scale_factor=1)
+        self.play(Indicate(rings[4]), run_time=2.0, color=BLUE, scale_factor=1)
+        self.wait(15)
         
         middlePoint = (op_box.get_center() + classification_box.get_center())/2
         interSymbol = Tex(r"$\bigcap$", color=WHITE).move_to(op_box.get_center()).scale(1.5)
         questionSymbol = Tex(r"$?$", color=WHITE).move_to(classification_box.get_center())
-        self.play(Write(interSymbol), run_time=1.0)
-        self.play(Write(questionSymbol), run_time=1.0)
+        
+        #2:13
+        self.play(Indicate(op_box), run_time=1.0, color=BLUE)
+        self.play(Indicate(classification_box), run_time=1.0, color=BLUE)
+        self.wait(3)
+        
+        #2:18
+        self.play(Write(interSymbol), run_time=1.5)
+        self.play(Write(questionSymbol), run_time=1.5)
         
         lenguage1 = Tex(r"$L_{1}$", color=WHITE).next_to(vennLenguages, RIGHT)
         lenguage2 = Tex(r"$L_{2}$", color=WHITE).next_to(vennLenguages, RIGHT)
         lenguage3 = Tex(r"$L_{3}$", color=WHITE).next_to(vennLenguages, RIGHT)
         
-        
+        #2:21
+        self.play(Indicate(rings[3]), run_time=1, color=BLUE, scale_factor=1)
         self.play(FadeIn(lenguage1, target_position=vennLenguages.get_center()), run_time=1.0)
         self.play(lenguage1.animate.next_to(op_box, LEFT), run_time=1.0)
-        self.play(FadeOut(lenguage1, shift=RIGHT), run_time=1.0)
+        self.play(FadeOut(lenguage1, shift=RIGHT), run_time=0.5)
         
         self.play(FadeIn(lenguage2, target_position=vennLenguages.get_center()), run_time=1.0)
         self.play(lenguage2.animate.next_to(op_box, LEFT), run_time=1.0)
-        self.play(FadeOut(lenguage2, shift=RIGHT), run_time=1.0)
+        self.play(FadeOut(lenguage2, shift=RIGHT), run_time=0.5)
         
         middlePoint = (op_box.get_center() + classification_box.get_center())/2
         lenguage3.move_to(middlePoint)
         
+        #2:27
         self.play(FadeIn(lenguage3, shift=DOWN), run_time=1.0)
         
         self.play(VGroup(classification_box, questionSymbol).animate.set_opacity(0.2), run_time=1.0)
@@ -217,14 +246,17 @@ class Video(Scene):
         lenguage3b = Tex(r"$?$", color=WHITE).next_to(lenguage3, RIGHT)
         self.play(Write(lenguage3b), run_time=1.0)
         
+        #2:31
+        for i in range(12):
+            self.play(Indicate(VGroup(lenguage3, lenguage3b)), run_time=2.0, color=YELLOW)
         
-        self.play(Indicate(VGroup(lenguage3, lenguage3b)), run_time=2.0, color=YELLOW)
-        self.wait(2)
+        self.wait(5)
         
         mobjects = self.mobjects
         copyOfIntBox = VGroup(classification_box, questionSymbol).copy()
         self.add(copyOfIntBox)
         
+        #3:00
         self.play(AnimationGroup(*[
             obj.animate(lag_ratio=0, run_time=1.5).set_opacity(0)
             for obj in mobjects
@@ -233,20 +265,50 @@ class Video(Scene):
         self.play(copyOfIntBox.animate(lag_ratio=0, run_time=1.5).move_to(ORIGIN))
         self.play(copyOfIntBox[1].animate(lag_ratio=0, run_time=1.5).set_opacity(0))
         self.play(copyOfIntBox.animate(lag_ratio=0, run_time=1.5).scale(15))
-        self.play(copyOfIntBox.animate(lag_ratio=0, run_time=0.5).set_opacity(0))
-        
-        self.play(closureTable.create(line_animation=Write,
-                            label_animation=Write,
-                            element_animation=Create),
-                            run_time=3)
+        self.play(copyOfIntBox.animate(lag_ratio=0, run_time=1).set_opacity(0))
         self.wait(2)
         
         
+        #3:09
+        self.play(closureTable.create(line_animation=Write,
+                            label_animation=Write,
+                            element_animation=Create),
+                            run_time=4)
+        self.wait(2)
         
-        self.play(Indicate(closureTable.get_entries((6,4)), color=YELLOW), run_time=1.0)
-        self.play(Indicate(closureTable.get_entries((3,4)), color=YELLOW), run_time=1.0)
+        #3:15
+        self.play(Indicate(closureTable.get_rows()[0]), color=YELLOW, run_time=1.0, scale_factor = 1.1)
+        self.play(Indicate(closureTable.get_columns()[0]), color=YELLOW, run_time=1.0, scale_factor = 1.1) 
+        self.wait(2)
+        self.play(Indicate(closureTable.get_entries_without_labels()), color=YELLOW, run_time=1.0, scale_factor = 1.1)
+        self.wait(5)
         
+        #3:25
+        self.play(Indicate(closureTable.get_entries((6,4))), run_time=1.0)
+        self.wait(2)
+        self.play(Indicate(closureTable.get_entries((3,4))), run_time=1.0)
+        self.wait(4)
+        
+        #3:33
+        self.play(Indicate(closureTable.get_entries((6,4)), color=BLUE), run_time=2.0)
+        self.wait(2)
+        self.play(Indicate(closureTable.get_entries((3,4)), color=BLUE), run_time=2.0)
+        self.wait(6)
+        
+        #3:45
         self.play(closureTable.animate.shift(UP), run_time=1.0)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # TODO
+        
         #Cells containing "No" are highlighted in red
         cellsContainingNo = [(3,4), (3,5), (5,2), (5,4), (5,5), (6,5), (7,5), (8,5), (9,5)]
         highlightedCells = VGroup()
--- README.md
+++ README.md
@@ -4,8 +4,11 @@ Estos videos han sido realizados utilizando principalmente la libreria [Manim](h
 
 Más concretamente, se ha utilizado la versión [Manim Community Edition](https://github.com/ManimCommunity/manim/), un fork de la versión original de 3Blue1Brown pensado para ser más estable, mantenido por un grupo de desarrolladores independientes.
 
-**Otras herramientas utilizadas:**
+**Otras herramientas utilizadas y enlaces de interés:**
 
 - [Latex Equation Editor](https://latexeditor.lagrida.com/) para generar fácilmente las ecuaciones y símbolos matemáticos.
+- [Documentación de Manim](https://docs.manim.community/en/stable/reference.html) para consultar la sintaxis de la libreria.
+- [Manim Color Reference](https://docs.manim.community/en/stable/reference/manim.utils.color.Colors.html) para consultar los colores disponibles en Manim.
+- [Audacity](https://www.audacityteam.org/) para grabar y editar el audio.
 
-## Video1 : APARTADO 2.11 Cierre de los Tipos de Lenguajes
+## Video1 : Apartado: 2.11-Cierre de los Tipos de Lenguajes
